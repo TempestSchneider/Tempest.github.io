c23e30f00414d9f4c67f5ead89838c0553b81f1a
TempestSchneider <link.exe8@gmail.com>
2024-05-08 23:10:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function change_page(){
  window.location.href = "2nd page.html";
 }
 function change_page_back(){
- window.location.href = "Monke.html.html";
+ window.location.href = "Monke.html";
 }
 
