0166eb229554aecb9c15e326d4dc7e6cc0b2a80c
TempestSchneider <link.exe8@gmail.com>
2024-05-08 23:05:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html> 
+
+<html> 
+
+<head> 
+
+function change_page(){
+ window.location.href = "2nd page.html";
+}
+function change_page_back(){
+ window.location.href = "Monke.html.html";
+}
+</body> 
+
+</html> 
+
+  
+
